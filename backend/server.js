70107aaf2a28a5c10e6e0d38b616a867c53767c5
SK-Lucifer-007 <srikanth2kboy@gmail.com>
2026-06-19 15:48:30 +0530

    not found error

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use('/api/todos', todoRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
